509ffb6aaf85276aac2bcb4b1c3e87754855b9f3
Davi <davihugomp@gmail.com>
2021-02-16 18:38:20 -0300

    Implemented wifi connection methods

--- lib/WNM/WNM.cpp
+++ lib/WNM/WNM.cpp
@@ -2,8 +2,10 @@
 
 using namespace WNM;
 
-std::vector<String> Wifi::getWifiList(){
-    int countWifi = WiFi.scanNetworks();
+std::vector<String> Wifi::getList(){
+    Serial.println("Getting wifiList");
+    openWifi = 0;
+    countWifi = WiFi.scanNetworks();
     std::vector<String> list = {};
     for(int i=0; i<countWifi; i++){
         Serial.print(i + 1);
@@ -16,9 +18,90 @@ std::vector<String> Wifi::getWifiList(){
             Serial.println("*");
         } else {
             Serial.println();
+            openWifi++;
         }
         list.push_back(WiFi.SSID(i));
     }
     wifiList = list;
     return wifiList;
+}
+
+bool Wifi::connect(){
+    String ssid;
+
+    if(wifiList.size()==0){
+        Serial.println("\nThe wifiList is empty!");
+        this->getList();
+    }
+
+    if (openWifi > 0) {
+        for (int i = 0; i < countWifi; ++i) {
+            if (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) {
+                Serial.println("\nBest open wifi available:");
+                Serial.print("SSID: ");
+                Serial.print(WiFi.SSID(i));
+                Serial.print("\t\tSIGNAL: ");
+                Serial.print(WiFi.RSSI(i));
+                Serial.print("\t\tCHANNEL: ");
+                Serial.println(WiFi.channel(i));
+                ssid = WiFi.SSID(i);
+                break;
+            }
+        }
+    } else {
+        Serial.println("There is no open Wifi.");
+        return false;
+    }
+
+    return this->connect(ssid.c_str());
+}
+
+bool Wifi::connect(const char *SSID){
+    WiFi.disconnect();
+    WiFi.begin(SSID);
+    return this->connectCheck(SSID);
+}
+
+bool Wifi::connect(const char *SSID, const char *Password){
+    WiFi.disconnect();
+    WiFi.begin(SSID, Password);
+    return this->connectCheck(SSID);
+}
+
+bool Wifi::connectCheck(const char *SSID){
+    Serial.print("Connecting to WiFi ");
+    Serial.print(SSID);
+    Serial.print('.');
+
+    int times = 0;
+
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        times++;
+        if (times == 250) break;
+        delay(100);
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+        wifiConnected = SSID;
+        Serial.println("\nWiFi connected!\n");
+        return true;
+    } else {
+        wifiConnected = "";
+        Serial.println("\nCouldn't connect.\n");
+        return false;
+    }
+}
+
+void Wifi::printInfo(){
+    if(wifiConnected == ""){
+        Serial.println("WiFi not connected!");
+    }else{
+        Serial.println("WiFi INFORMATIONS:");
+        Serial.println("- SSID: "+WiFi.SSID());
+        Serial.println("- SIGNAL: "+String(WiFi.RSSI()));
+        Serial.println("- CHANNEL: "+String(WiFi.channel()));
+        Serial.println("- IP: "+WiFi.localIP().toString());
+        Serial.println("- MAC: "+WiFi.macAddress());
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <Arduino.h>
-#include "WiFi.h"
 #include "WNM.h"
-#include <iostream>
-#include <string>
-
-#define LED_BUILTIN 2 
 
 //Global variables
 String wifiConnected = "";
 WNM::Wifi wifi;
-std::vector<String> x;
+char SSID[] = "Network_SSID";
+char PASS[] = "Network_PASS";
 
 //Functions declaration
 String WifiScanOpenNet();
@@ -17,22 +13,22 @@ void WifiConnect(String ssid);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  x = wifi.getWifiList();  
+  delay(500);
 
-  Serial.println("Setup");
+  wifi.connect(SSID, PASS);
+  wifi.printInfo();
+
+  Serial.setTimeout(2000);
 }
 
 void loop() {
+  /*
   if(Serial.available()){
     String y = Serial.readStringUntil('\n');
     Serial.println("String capturada: "+y);
     Serial.println(x[std::atoi(y.c_str())]);
   }
 
-  //delay(200000);
-
-  /*
   //Retorna o nome da rede aberta com melhor sinal
   String ssid = WifiScanOpenNet();
 
@@ -54,90 +50,4 @@ void loop() {
 
   delay(20000);
   */
-}
-
-/*--- VERIFICA REDES WIFI ABERTAS ---*/
-String WifiScanOpenNet() {
-  Serial.println("Procurando redes WiFi...");
-
-  int countWifi = WiFi.scanNetworks(); //Retorna a quantidade de redes wifi disponíveis
-  int openWifi = 0; //Variável para contar quantas redes abertas foram encontradas
-  String ssid = "";
-
-  //Exibe as redes wifi encontradas
-  if (countWifi > 0) {
-    Serial.println(String(countWifi) + " redes encontradas.\n");
-    delay(500);
-    for (int i = 0; i < countWifi; ++i) {
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(") ");
-      if (WiFi.encryptionType(i) != WIFI_AUTH_OPEN) {
-        Serial.println("*");
-      } else {
-        Serial.println();
-        openWifi++;
-      }
-    }
-  } else {
-    Serial.println("Nenhuma rede encontrada.");
-    return ssid;
-  }
-  delay(2000);
-
-  //Exibe os dados da rede wifi aberta com melhor sinal
-  if (openWifi > 0) {
-    for (int i = 0; i < countWifi; ++i) {
-      if (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) {
-        Serial.println("\nRede aberta com melhor sinal:");
-        Serial.print("SSID: ");
-        Serial.print(WiFi.SSID(i));
-        Serial.print("\t\tSINAL: ");
-        Serial.print(WiFi.RSSI(i));
-        Serial.print("\t\tCANAL: ");
-        Serial.println(WiFi.channel(i));
-        Serial.println();
-        ssid = WiFi.SSID(i);
-        break; //Finaliza o loop
-      }
-    }
-  } else {
-    Serial.println("\nNenhuma rede aberta encontrada.\n");
-  }
-  delay(2000);
-
-  return ssid;
-}
-
-/*--- EFETUA CONEXÃO COM REDE WIFI ---*/
-void WifiConnect(String ssid) {
-  char wifiSSID[50];
-  ssid.toCharArray(wifiSSID, 50);
-
-  WiFi.disconnect();    //Fecha conexão aberta
-  WiFi.begin(wifiSSID); //Efetua a conexão com a rede wifi
-
-  Serial.print("Conectando no WiFi " + ssid + ".");
-  int times = 0;
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    times++;
-    if (times == 250) break; //Mantém aguardando conexão por 15 segundos
-    delay(100);
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    wifiConnected = ssid;
-    Serial.println("\nWiFi conectado!\n");
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    wifiConnected = "";
-    Serial.println("\nNao foi possivel conectar.\n");
-    digitalWrite(LED_BUILTIN, LOW);
-  }
 }
